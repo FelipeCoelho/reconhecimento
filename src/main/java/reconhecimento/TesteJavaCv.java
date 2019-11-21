0e9e13d468bb559b6e073f63720e0d83827ae9aa
Felipe Coelho <felipeisrael@gmail.com>
2019-11-21 20:15:37 -0300

    primeiro commit

---
+++ src/main/java/reconhecimento/TesteJavaCv.java
@@ -0,0 +1,14 @@
+package reconhecimento;
+
+//import static org.bytedeco.javacpp.opencv_face.createEigenFaceRecognizer;
+//import static org.bytedeco.javacpp.opencv_face.createEigenFaceRecognizer;
+
+import org.bytedeco.opencv.opencv_face.FaceRecognizer;
+
+public class TesteJavaCv {
+
+	
+	public static void main(String[] args) {
+//		FaceRecognizer fr = createEigenFaceRecognizer();
+	}
+}
